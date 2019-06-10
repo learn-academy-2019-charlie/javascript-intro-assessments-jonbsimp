1a4d8f732c3de408c5774dcb8abd5072c11773b9
Ubuntu <ubuntu@ip-172-31-19-7.us-west-2.compute.internal>
2019-06-10 17:14:57 +0000

    added content

--- js-intro.js
+++ js-intro.js
@@ -6,17 +6,44 @@ var mantra = "Be the dev";
 
 // 1. Write the code that returns 'B' from mantra.
 
+const returnB = (mantra) => mantra.charAt(0)
+
+console.log(returnB(mantra))
+
+
 // 2. Write the code that determines if there is a 'x' in mantra.
+
+const findX = (mantra) => {
+    let x = mantra.indexOf('x')
+        if ( x === true) {
+            return "x in mantra"
+        } else {
+            return "no x in mantra"
+        }
+}
+
+console.log(findX(mantra))
 
 // 3. Write the code that determines if there is a 'v' in mantra.
 
-// Stretch: Write the code that returns the position of 'v' in mantra.
+const findV = (mantra) => {
+    let v = mantra.indexOf('v')
+        if ( v === true) {
+            return "v in mantra"
+        } else {
+            return "no v in mantra"
+        }
+}
 
+console.log(findV(mantra))
+
+
+// Stretch: Write the code that returns the position of 'v' in mantra.
 
 // Consider the following statement:
 
-var codingIsEasy
-var imBored
+var codingIsEasy = true
+var imBored = true
 
 if (codingIsEasy && imBored) {
     console.log("I need more!")
@@ -28,20 +55,25 @@ if (codingIsEasy && imBored) {
 
 // Consider the following statement:
 
-var imLost
-var imFrustrated
+var imLost = false
+var imFrustrated = false 
 
 if (imLost || imFrustrated) {
     console.log('Break time!')
 } else {
-    console.log('Keep coding!')
+    console.log('Keep coding! \n')
 }
 
 // 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
 
 // 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
 
+for (let i=0; i < mantra.length; i++) {
+    console.log(mantra.charAt(i))
+}
+
 // Stretch: Using a while loop.
+
 
 // Super Stretch: Using forEach().
 
@@ -49,7 +81,14 @@ if (imLost || imFrustrated) {
 
 var message = "thisisateststring"
 
+
 // 7. Write a function that takes a string like message as an argument and returns the string without vowels.
+
+
+const removeVowels = string => string.replace(/[aeiou]/gi, '');
+
+console.log(removeVowels(message))
+
 
 // Stretch: Update your function to throw an exception if the argument is not a string.
 
@@ -60,3 +99,14 @@ var message = "thisisateststring"
 var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
 
 // 8. Write a function that takes an array like toonimals and returns an array with only the toons that are cats.
+
+
+const catsOnly = (list) => {
+    let holder = []
+    for(let i=0; i<toonimals.length; i++)
+        if(toonimals[i].animal === 'cat') {
+            holder.push(toonimals[i])
+        } return holder
+}
+
+ console.log(catsOnly(toonimals))
--- js-intro.md
+++ js-intro.md
@@ -4,14 +4,33 @@ Answer these questions, then follow the instructions to submit your first assess
 
 #### 1. What is a function? Why would you use one?
 
-        Put sentence here
+        - A function is a Javascript procedure- a set of statements that perform a task or calculate a value. To use a function,
+        you must define it somewhere in the scope from which you wish to call it.
 
 #### 2. What is a method?
 
+        - Methods are actions that can be performed on objects (belongs to a class). It is a property containing a function definition. 
+
 #### 3. What are some examples of javascript string methods?
 
+        - charAt()      : Returns the character at the specified index (position)
+        - concat()      : Joins two or more strings
+        - indexOf()     : Returns the position of the first round occurence of a specified value in a string
+        - lastIndexOf() : Returns the position of the last round occurence of a specified value in a string
+        - includes()    : Checks whether a string contains the specified string or characters
+        - toUpperCase() : Converts a string to uppercase letters
+        - toLowerCase() : Converts a string to lowercase letters
+        
 #### 4. Explain the difference between accessor and mutator methods. Why would you use one over the other?
+
+        - Mutator methods modify the original array and accessors return a new value or representation.
 
 #### 5. Explain the differences between console.log() and return. Why would you use one over the other?
 
-#### 6. What is one thing we did in class this week that helped you learn the most?  
+        - console.log : logs out information in console
+        - return      : returns execution to the caller with optional result 
+        
+#### 6. What is one thing we did in class this week that helped you learn the most? 
+
+        - Doing problems on the projector as a class really helps me. I'm more of a visual learner, so seeing 
+          Damon or Sarah do examples is a lot more beneficial for me than trying to read instructions on my own. 
